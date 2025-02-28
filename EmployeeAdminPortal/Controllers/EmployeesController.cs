6729c6f6c9155923d759e6806fe4c35b8e0ca53b
Raul <32076988+RaulJCS5@users.noreply.github.com>
2025-02-28 14:47:37 -0100

    Employee Controller

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,85 @@
+﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    // localhost:xxxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = _context.Employees.ToList();
+            return Ok(allEmployees);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee([FromBody] AddEmployeeDto employee)
+        {
+            var newEmployee = new Employee
+            {
+                Name = employee.Name,
+                Email = employee.Email,
+                Phone = employee.Phone,
+                Salary = employee.Salary
+            };
+            _context.Employees.Add(newEmployee);
+            _context.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id, [FromBody] UpdateEmployeeDto employee)
+        {
+            var existingEmployee = _context.Employees.Find(id);
+            if (existingEmployee is null)
+            {
+                return NotFound();
+            }
+            existingEmployee.Name = employee.Name;
+            existingEmployee.Email = employee.Email;
+            existingEmployee.Phone = employee.Phone;
+            existingEmployee.Salary = employee.Salary;
+            _context.SaveChanges();
+            return Ok(existingEmployee);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id) {
+            var employee = _context.Employees.Find(id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
